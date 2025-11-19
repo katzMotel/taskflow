599794b6e39aadad925b69d22c7ccfeaf93e8479
Dylan Giddens <74880352+katzMotel@users.noreply.github.com>
2025-11-18 21:11:13 -0600

    created sidebar link menu with tailwind styling, dear lord this is kinda hard to read compared to a seperate css or scss file but i guess its clearner

--- src/app/shared/components/sidebar/sidebar.component.ts
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -1,11 +1,29 @@
 import { Component } from '@angular/core';
+import { RouterLink ,RouterLinkActive} from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { LucideAngularModule , Home, BarChart3, KanbanSquare, CheckSquare, Settings, LucideIconData} from 'lucide-angular';
+interface NavLink {
+  name: string;
+  route: string;
+  icon: LucideIconData;
+}
 
 @Component({
   selector: 'app-sidebar',
-  imports: [],
+  standalone: true,
+  imports: [RouterLink, RouterLinkActive, LucideAngularModule, CommonModule],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.css'
 })
-export class SidebarComponent {
 
+
+export class SidebarComponent {
+  // an array for sidebar items
+  sidebarLinks: NavLink[] = [
+    {name: 'Dashboard', route: '/dashboard',icon:Home},
+    {name: 'Analytics', route: '/analytics',icon:BarChart3},
+    {name: 'Kanban', route: '/kanban',icon:KanbanSquare},
+    {name: 'Tasks', route: '/tasks',icon:CheckSquare},
+    {name: 'Settings', route: '/settings',icon:Settings}
+  ]
 }
